a6b98aea70f2db46bc6680bbb3369d47ddb1a310
revanth <revanthkumar.017@gmail.com>
2020-07-28 21:50:47 +0530

    devlop branch in git demo

--- src/test/java/Revanth/SeleniumTest.java
+++ src/test/java/Revanth/SeleniumTest.java
@@ -9,6 +9,12 @@ public class SeleniumTest {
 	{
 		System.out.println("sample");
 		System.out.println("edited1");
+		System.out.println("sample");
+		System.out.println("edited2");
+		System.out.println("sample");
+		System.out.println("edited3");
+		System.out.println("sample");
+		System.out.println("edited4");
 	}
 
 }
